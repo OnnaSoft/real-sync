cb6fde090742c14c90f0842df42171d6900b0edc
Julio Torres <jtorres990@gmail.com>
2024-12-16 19:29:40 -0500

    Refactor: Update S3 configuration to use consistent variable naming for bucket name and remove unnecessary comments in uploadFileToS3 function

--- server/services/s3Service.ts
+++ server/services/s3Service.ts
@@ -1,10 +1,10 @@
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { s3Client, S3_BUCKET_NAME } from "../config/s3Config";
+import { s3Client, s3BucketName } from "&/config/s3Config";
 
 export async function uploadFileToS3(file: Express.Multer.File, key: string): Promise<string> {
   const command = new PutObjectCommand({
-    Bucket: S3_BUCKET_NAME,
+    Bucket: s3BucketName,
     Key: key,
     Body: file.buffer,
     ContentType: file.mimetype,
@@ -12,7 +12,6 @@ export async function uploadFileToS3(file: Express.Multer.File, key: string): Pr
 
   await s3Client.send(command);
 
-  // Generate a signed URL for the uploaded object
   const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
 
   return signedUrl;
